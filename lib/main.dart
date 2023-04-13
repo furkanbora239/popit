25e4a6dd6607208cdb183e0a41c01590ece59e85
furkan bora <frknbora@hotmail.com>
2023-04-13 08:58:41 +0300

    ready for lounch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,20 +17,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const popit(),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Container(
+            decoration: const BoxDecoration(color: Colors.red),
+            child: const popitGrid()));
+  }
+}
+
+class popitGrid extends StatelessWidget {
+  const popitGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: (MediaQuery.of(context).size.width / 160).floor()),
+      itemBuilder: (context, index) {
+        return const popit();
+      },
+      physics: const ScrollPhysics(parent: NeverScrollableScrollPhysics()),
+      itemCount: (MediaQuery.of(context).size.width / 160).floor() *
+          (MediaQuery.of(context).size.height / 160).floor(),
     );
   }
 }
@@ -40,14 +63,20 @@ class popit extends StatefulWidget {
 }
 
 class _popitState extends State<popit> {
+  final player = AudioPlayer();
   @override
   void initState() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-
     super.initState();
   }
 
   bool buble = true;
+
+  void popSound() async {
+    player.setAsset('assets/popSound.mp3');
+    const Duration(milliseconds: 120);
+    await player.play();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,15 +86,19 @@ class _popitState extends State<popit> {
         Container(
           height: 160,
           width: 160,
-          decoration: BoxDecoration(color: Colors.red),
+          decoration: const BoxDecoration(color: Colors.red),
         ),
         GestureDetector(
           onTapDown: (details) {
             setState(() {
               buble = false;
               HapticFeedback.lightImpact();
+              popSound();
             });
           },
+          onTapCancel: () => setState(() {
+            buble = true;
+          }),
           onTapUp: (details) {
             setState(() {
               buble = true;
@@ -73,8 +106,8 @@ class _popitState extends State<popit> {
           },
           child: AnimatedContainer(
             duration: const Duration(milliseconds: 120),
-            height: 140,
-            width: 140,
+            height: 120,
+            width: 120,
             decoration: BoxDecoration(
                 color: Colors.green,
                 shape: BoxShape.circle,
@@ -82,14 +115,14 @@ class _popitState extends State<popit> {
                     ? [
                         const BoxShadow(
                             offset: Offset(-6, -6),
-                            color: Color.fromARGB(118, 255, 255, 255),
-                            blurRadius: 15,
-                            spreadRadius: 5),
+                            color: Color.fromARGB(53, 255, 255, 255),
+                            blurRadius: 7,
+                            spreadRadius: 3),
                         BoxShadow(
                             offset: const Offset(6, 6),
                             color: Colors.red.shade800,
-                            blurRadius: 15,
-                            spreadRadius: 5)
+                            blurRadius: 7,
+                            spreadRadius: 3)
                       ]
                     : [],
                 gradient: LinearGradient(
